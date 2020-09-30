4252472e597d72feb083955c855afdcc6f5dffe7
Mathieu Brujan <ken@localhost.localdomain>
2020-09-30 15:06:12 -0400

    Adding the print comb

---
+++ my_print_comb.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2020
+** my_print_comb
+** File description:
+** Function that diplays, in ascending order, all the numbers composed by three different digits numbers  (only the smallest number composed bu those digits must be displayed
+*/
+
+int my_print_comb(void)
+{
+    int a;
+    int b;
+    int c;
+    int n;
+
+    n = 0;
+    while (n<=789) {
+        a = n % 10;
+        b = n / 10 % 10;
+        c = n / 100 % 10;
+    if (c < b && b < a) {
+        my_putchar(c+48);
+        my_putchar(b+48);
+        my_putchar(a+48);
+
+        if (n<789) {
+            my_putchar(',');
+            my_putchar(' ');
+        }
+    }   
+    n++;           
+    }
+    return (0);
+}
+        
