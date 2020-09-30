cbc73f46be435c0fe4e3e82ef60989224432fd4d
Mathieu Brujan <ken@localhost.localdomain>
2020-09-30 08:37:50 -0400

    Edition of looneytised

--- looneytised.sh
+++ looneytised.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-cat Day02/passwd | grep 'theo1\|steven1\|arnaud1\|pierre-jean' | sed 's/theo1/wile e. coyote/gi' | sed 's/steven1/daffy duck/gi' | sed 's/arnaud1/porky pig/gi' | sed 's/pierre-jean/marvin the martian/gi'
+grep 'theo1\|steven1\|arnaud1\|pierre-jean' | sed 's/theo1/Wile E. Coyote/gi' | sed 's/steven1/Daffy Duck/gi' | sed 's/arnaud1/Porky Pig/gi' | sed 's/pierre-jean/Marvin the Martian/gi'
