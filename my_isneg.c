ef2719c3e9ef7b705e5f1e9a062b6d4dab9ec9e3
Mathieu Brujan <ken@localhost.localdomain>
2020-09-30 07:44:26 -0400

    Pushing isneg

---
+++ my_isneg.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2020
+** my_isneg
+** File description:
+** Function that displays either N if the interger passed as negative, P if positive or null
+*/
+
+int     my_isneg(int n)
+{
+    char N = 'N';
+    char P = 'P';
+        
+    if (n >= 0)
+    {
+        my_putchar(P);
+    }
+
+    else
+    {
+        my_putchar(N);
+    }
+}
