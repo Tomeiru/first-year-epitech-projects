e3082699f7b39732f58bb8b382bc6a88a5189b10
Mathieu Brujan <ken@localhost.localdomain>
2020-10-01 14:30:59 -0400

    FINALLY

--- my_print_comb2.c
+++ my_print_comb2.c
@@ -7,44 +7,32 @@
 
 int my_print_comb2(void)
 {
-    int a;
-    int b;
-    int m = 0;
-    int y;
-    int z;
-    int n = 1;
 
-    while (m<=99) {
+    int m = 1;
+    int a = 0;
+    int b = 0;
+    int z = 0;
+    int y = 0;
+    int n = 0;
+    
+    while (m <= 99) {
         a = m % 10;
         b = m / 10 % 10;
+        z = n % 10;
+        y = n / 10 % 10;
         m++;
-        my_putchar (48);
-        my_putchar (48);
+        my_putchar (y + 48);
+        my_putchar (z + 48);
         my_putchar (' ');
         my_putchar (b + 48);
         my_putchar (a + 48);
-        my_putchar (',');
-        my_putchar (' ');
-    }
-    while (n<=98) {
-        y = n % 10;
-        z = n / 10 % 10;
-        n++;
-        my_putchar (z + 48);
-        my_putchar (y + 48);
-        my_putchar (' ');
-        my_putchar (z + 48);
-        my_putchar (y + 49);
-        my_putchar (',');
-        my_putchar (' ');
-        my_putchar (z + 48);
-        my_putchar (y + 48);
-        my_putchar (' ');
-        my_putchar (b + 48);
-        my_putchar (b + 48);
-        if(n <= 98) {
-        my_putchar (',');
-        my_putchar (' ');
+        if (n < 98){
+            my_putchar (',');
+            my_putchar (' ');
+        }
+        if (m == 100 && n < 98) {
+            n++;
+            m = n + 1;
         }
     }
 }
