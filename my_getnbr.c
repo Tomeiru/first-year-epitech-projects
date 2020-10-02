13713f012a90e949cd5434fff6c1d3b8a75ce7d1
Mathieu Brujan <ken@localhost.localdomain>
2020-10-02 14:22:03 -0400

    ENFIN

---
+++ my_getnbr.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2020
+** my_getnbr
+** File description:
+** my_getnbr
+*/
+
+int my_getnbr (char const *str)
+{
+    long number = 0; 
+    int pos = 1;
+    int l = 0;
+
+    while (str[l] == '-' || str[l] == '+'){
+        if (str[l] == '-') {
+            pos = pos * -1;
+        }
+        l++;
+    }        
+    while (str[l] >= '0' && str[l] <= '9') {
+        number = number * 10;
+        number = number + str[l] - '0';
+        l++;
+    }
+    number = number * pos;
+    if (number > 2147483647 || number > -2147483547) {
+        return (0);
+    }
+    return((int)number);
+}
