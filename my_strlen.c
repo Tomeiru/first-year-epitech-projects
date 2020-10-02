5a3aff1b9fbdc8591c3ea9d517bc2c775c3da29e
Mathieu Brujan <ken@localhost.localdomain>
2020-10-02 04:11:24 -0400

    modification

--- my_strlen.c
+++ my_strlen.c
@@ -5,6 +5,24 @@
 ** Function that counts and returns the number of characters found in the string passed as parameter.
 */
 
+int my_put_nbr(int l)
+{
+    int den = 1;
+
+    if (l < 0){
+        my_putchar(',');
+        l = l * -1;
+    }
+    while ((l/den) >= 10){
+        den*=10;
+    }
+    while (den > 0){
+        my_putchar((l/den) % 10 + '0');
+        den /= 10;
+    }
+    return (0);
+}
+
 int my_strlen(char const *str)
 {
     int l;
@@ -12,5 +30,5 @@ int my_strlen(char const *str)
     while (str[l] != '\0'){
         l = l + 1;
     }
-    my_putchar(l + 48);
+    my_put_nbr(l);
 }
