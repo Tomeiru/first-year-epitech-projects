b1453671d6552c8b86c2b3793c37d402ce02e9b9
Mathieu Brujan <ken@localhost.localdomain>
2020-10-03 09:18:57 -0400

    pushing assign 2

---
+++ rush-1-2/rush.c
@@ -0,0 +1,94 @@
+/*
+** EPITECH PROJECT, 2020
+** rush
+** File description:
+** rush
+*/
+
+void top_line(int a)
+{
+    int amax = a;
+
+    if (a == amax) {
+        my_putchar('/');
+        a--;
+    }
+    while (a < amax && a > 1) {
+        my_putchar('*');
+        a--;
+    }
+    if (a == 1)
+        my_putchar(92);
+    my_putchar('\n');
+}
+
+void between_lines (int b, int c)
+{
+    c = c -2;
+    int bmax = b;
+
+    while (c > 0) {
+        if (b == bmax)
+            my_putchar('*');
+        while (b != 2 && b > 0) {
+            my_putchar (' ');
+            b--;
+        }
+        b--;
+        if(b == 1)
+            my_putchar('*');
+        c--;
+        b = bmax;
+        my_putchar('\n');
+    }
+}
+
+void bot_line(int d)
+{
+    int dmax = d;
+
+    if(d == dmax) {
+        my_putchar(92);
+        d--;
+    }
+    while (d < dmax && d > 1) {
+        my_putchar('*');
+        d--;
+    }
+    if (d == 1)
+        my_putchar('/');
+    my_putchar('\n');
+}
+
+void exception(int e, int f)
+{
+    if(e == 1 && f > 1)
+        while(f != 0) {
+            my_putchar('*');
+            my_putchar('\n');
+            f--;
+        }
+    if(e > 1 && f == 1) {
+        while(e != 0) {
+            my_putchar('*');
+            e--;
+        }
+        my_putchar('\n');
+    }
+}
+
+void rush(int x, int y)
+{
+    if(x == 1 && y == 1) {
+        my_putchar('*');
+        my_putchar('\n');
+    }
+    if((x == 1 && y > 1) || ( x > 1 && y == 1))
+        exception(x, y);
+    if(x > 1 && y > 1) {
+        top_line(x);
+        between_lines(x,y);
+        if (y >=  2)
+        bot_line(x);
+    }
+}
