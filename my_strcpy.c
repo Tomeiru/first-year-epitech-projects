a9ee6cbbe6c9138dd00ee0a05af2b2d120544c2f
Mathieu Brujan <ken@localhost.localdomain>
2020-10-05 10:22:18 -0400

    oui

--- my_strcpy.c
+++ my_strcpy.c
@@ -9,7 +9,8 @@ char *my_strcpy(char *dest, char const *src)
 {
     int i = 0;
     
-    for (i = 0; src[i] != '\0'; i++)
+    for ( ; src[i] != '\0'; i++)
         dest [i] = src [i];
+    dest[i] = '\0';
     return (dest);
 }
