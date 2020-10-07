cc67b19f5d10b3d930cf1ce8379b7ec95c2d09f9
Mathieu Brujan <ken@localhost.localdomain>
2020-10-07 05:43:55 -0400

    a lot of things

---
+++ lib/my/my_compute_power_rec.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2020
+** my_compute_power
+** File description:
+** my_compute_power
+*/
+
+int my_compute_power_rec(int nb, int p)
+{
+    long res = 0;
+
+    if (res > 2147483647 || res > 2147483647)
+        return (0);
+    if (res < -2147483647 || res < -2147483647)
+        return (0);
+    if (p < 0)
+        return (0);
+    if (p == 0)
+        return (1);
+
+    res = nb * my_compute_power_rec(nb, p - 1);
+
+    return ((int)res);
+}
