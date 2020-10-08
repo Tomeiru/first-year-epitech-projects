19621fa0c561f5ba138e0c9941bb35163c300ff0
Mathieu Brujan <ken@localhost.localdomain>
2020-10-08 04:18:47 -0400

    taskuno

---
+++ lib/my/my_putstr.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** my_putstr
+** File description:
+** my_putstr
+*/
+
+int my_putstr(char const *str)
+{
+    int l;
+
+    while (str[l] != '\0'){
+        my_putchar(str[l]);
+        l = l + 1;
+    }
+}
