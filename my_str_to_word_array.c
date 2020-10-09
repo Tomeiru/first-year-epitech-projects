a3858aa489cc76255aa6268ffcb6a21784a82e43
Mathieu Brujan <ken@localhost.localdomain>
2020-10-09 10:41:36 -0400

    time to norm

--- my_str_to_word_array.c
+++ my_str_to_word_array.c
@@ -38,7 +38,9 @@ int ligncounter (char const *str)
     for (char_i = 0; str[char_i] != '\0'; char_i++) {
         if (str[char_i] > 57 && str [char_i] < 65 || str[char_i] > 90 &
         & str[char_i] < 97 || str[char_i] > 122 || str[char_i] < 48) {
-            totalign++;
+            if (str[char_i + 1] > 47 && str[char_i + 1] < 58 || str[char_i + 1] > 64 &&
+                str[char_i + 1] < 91 || str[char_i + 1] > 96 && str[char_i + 1] < 123)
+                totalign++;
         }
     }
     totalign += 2;
@@ -52,15 +54,17 @@ char **my_str_to_word_array(char const *str)
     int string_i = 0;
     int charmax = charcounter(str);
     char **wordarray;
-    
+
+    if (str[char_i] == ' ')
+        string_i = -1;
     wordarray = malloc(sizeof(char*) * ligncounter(str));
     wordarray[string_i] = malloc(sizeof(char) * charmax);
     for (char_i = 0; str[char_i] != '\0'; char_i++) {
-        if (str[char_i] > 57 && str [char_i] < 65 || str[char_i] > 90 &
-            & str[char_i] < 97 || str[char_i] > 122 || str[char_i] < 48) {
-                if (str[char_i + 1] > 47 && str[char_i + 1] < 58 || str[char_i + 1] > 64 &
-                & str[char_i + 1] < 91 || str[char_i + 1] > 96 && str[char_i + 1] < 123) {
-                     wordarray[string_i][char_ia] = '\0';
+        if (str[char_i] > 57 && str [char_i] < 65 || str[char_i] > 90 &&
+            str[char_i] < 97 || str[char_i] > 122 || str[char_i] < 48) {
+                if ((str[char_i + 1] > 47 && str[char_i + 1] < 58 || str[char_i + 1] > 64 &&
+                str[char_i + 1] < 91 || str[char_i + 1] > 96 && str[char_i + 1] < 123)) {
+                    wordarray[string_i][char_ia] = '\0';
                     string_i++;
                     wordarray[string_i] = malloc(sizeof(char) * charmax);
                     char_ia = 0;
