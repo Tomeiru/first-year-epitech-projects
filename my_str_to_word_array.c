b12c05f452257b80dbf06a525a44b4b1da11d580
Mathieu Brujan <ken@localhost.localdomain>
2020-10-09 11:28:28 -0400

    norm a bit better

--- my_str_to_word_array.c
+++ my_str_to_word_array.c
@@ -7,15 +7,30 @@
 
 #include <stdlib.h>
 
-int charcounter (char const *str)
+int my_isnotalphanumerical(char d)
+{
+    int count = 0;
+    
+    if (d > 57 && d < 65 || d > 90 &&
+    d < 97 || d > 122 || d < 48) {
+        count++;
+    }
+    if (count == 0)
+        return (1);
+    else
+        return (0);
+}
+
+int charcounter(char const *str)
 {
     int maxchar = 0;
     int totalchar = 0;
     int char_i = 0;
 
     for (char_i = 0; str[char_i] != '\0'; char_i++) {
-        if (str[char_i] > 57 && str [char_i] < 65 || str[char_i] > 90 &
-        & str[char_i] < 97 || str[char_i] > 122 || str[char_i] < 48) {
+        if (my_isnotalphanumerical(str[char_i]) == 0)
+            totalchar++;
+        else {
             if (totalchar + 1 > maxchar) {
                 totalchar++;
                 maxchar = totalchar;
@@ -23,23 +38,18 @@ int charcounter (char const *str)
             }
             totalchar = 0;
         }
-        else {
-            totalchar++;
-        }
     }
     return (maxchar);
 }
 
-int ligncounter (char const *str)
+int ligncounter(char const *str)
 {
     int totalign = 0;
     int char_i = 0;
 
     for (char_i = 0; str[char_i] != '\0'; char_i++) {
-        if (str[char_i] > 57 && str [char_i] < 65 || str[char_i] > 90 &
-        & str[char_i] < 97 || str[char_i] > 122 || str[char_i] < 48) {
-            if (str[char_i + 1] > 47 && str[char_i + 1] < 58 || str[char_i + 1] > 64 &&
-                str[char_i + 1] < 91 || str[char_i + 1] > 96 && str[char_i + 1] < 123)
+        if (my_isnotalphanumerical(str[char_i]) == 0) {
+            if (my_isnotalphanumerical(str[char_i + 1]) == 1)
                 totalign++;
         }
     }
@@ -60,18 +70,16 @@ char **my_str_to_word_array(char const *str)
     wordarray = malloc(sizeof(char*) * ligncounter(str));
     wordarray[string_i] = malloc(sizeof(char) * charmax);
     for (char_i = 0; str[char_i] != '\0'; char_i++) {
-        if (str[char_i] > 57 && str [char_i] < 65 || str[char_i] > 90 &&
-            str[char_i] < 97 || str[char_i] > 122 || str[char_i] < 48) {
-                if ((str[char_i + 1] > 47 && str[char_i + 1] < 58 || str[char_i + 1] > 64 &&
-                str[char_i + 1] < 91 || str[char_i + 1] > 96 && str[char_i + 1] < 123)) {
-                    wordarray[string_i][char_ia] = '\0';
-                    string_i++;
-                    wordarray[string_i] = malloc(sizeof(char) * charmax);
-                    char_ia = 0;
-                }
-                else {
-                   char_ia++;
-                }
+        if (my_isnotalphanumerical(str[char_i]) == 0) {
+            if (my_isnotalphanumerical(str[char_i + 1]) == 1) {
+                wordarray[string_i][char_ia] = '\0';
+                string_i++;
+                wordarray[string_i] = malloc(sizeof(char) * charmax);
+                char_ia = 0;
+            }
+            else {
+                char_ia++;
+            }
         }
         else {
             wordarray[string_i][char_ia] = str[char_i];
