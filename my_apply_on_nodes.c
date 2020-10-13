f9f11fcfd462420cddbd3f01a1f896aca7ae1ace
Mathieu Brujan <ken@localhost.localdomain>
2020-10-13 13:15:14 -0400

    yeee

---
+++ my_apply_on_nodes.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2020
+** my_apply_on_nodes
+** File description:
+** my_apply_on_nodes
+*/
+
+#include <stddef.h>
+#include "mylist.h"
+
+int my_apply_on_nodes(linked_list_t *begin , int (*f)(void *))
+{
+    int count = 0;
+
+    while (begin != NULL) {
+        (*f)(begin->data);
+        begin = begin->next;
+    }
+}
