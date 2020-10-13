15a0a6668f1359b8ba4f68e46b5b7fbc881a9282
Mathieu Brujan <ken@localhost.localdomain>
2020-10-13 07:19:12 -0400

    yes

---
+++ my_params_to_list.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2020
+** my_params_to_list
+** File description:
+** my_params_to_list
+*/
+
+#include <stddef.h>
+
+linked_list_t *pleaselist(linked_list_t *list, char *s)
+{
+    linked_list_t *prev = malloc(sizeof(linked_list_t));
+    prev->data = s;
+    prev->next = list;
+    list = prev;
+}
+
+linked_list_t *my_params_to_list(int ac, char * const *av)
+{
+    linked_list_t *list = NULL;
+    for(int i = 0; i < ac; i++) {
+        pleaselist(list, av[i]);
+    }
+    return (list);
+
+}
