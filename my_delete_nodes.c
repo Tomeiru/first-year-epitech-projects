db7c6e0d0ff8f6515d25506d2a58afbf67936811
Mathieu Brujan <ken@localhost.localdomain>
2020-10-14 09:53:40 -0400

    ye

---
+++ my_delete_nodes.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2020
+** my_delete_nodes
+** File description:
+** my_delete_nodes
+*/
+
+#include <stddef.h>
+#include "mylist.h"
+
+int my_delete_nodes(linked_list_t **begin , void const *data_ref , int (*cmp)())
+{
+    linked_list_t *list = *begin;
+
+    while (begin != NULL) {
+        if (cmp(list->data, data_ref))
+            *begin = list->next;
+        list = list->next;
+    }
+    *begin = list;
+}
