1a2f867048d715075d820e1186d2ec24cd665d6e
Mathieu Brujan <ken@localhost.localdomain>
2020-10-14 08:25:31 -0400

    ue

---
+++ my_find_nodes.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** my_find_nodes
+** File description:
+** my_find_nodes
+*/
+
+#include <stddef.h>
+#include "mylist.h"
+
+int my_find_nodes(linked_list_t const *begin , int (*f)(), void const *data_ref, int (*cmp)())
+{
+    while (begin != NULL) {
+        if (cmp(begin->data, data_ref))
+            return (begin);
+        begin = begin->next;
+    }
+}
