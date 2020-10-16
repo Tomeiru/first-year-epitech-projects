2b3be22bbf587540edc627850e0fd29dbf52df21
Mathieu Brujan <ken@localhost.localdomain>
2020-10-16 11:20:33 -0400

    error

--- error.c
+++ error.c
@@ -10,14 +10,18 @@
 
 int error (int ac, char **av)
 {
+    int i = 0;
+    int j = 0;
+
     if (is_valid_num(av[1]) == 0 || is_valid_num(av[2]) == 0)
         return (84);
     if (av[1][0] == '\0' || av[2][0] == '\0')
         return (84);
     if (ac != 3)
         return (84);
-    if ((my_strlen(av[1]) == 1 && av[1][0] == '0') && (my_strlen(av[2]) == 1 
-    && av[2][0] == '0'))
+    for (j = 0; av[2][j] == '0'; j++);
+    for (i = 0; av[1][i] == '0'; i++);
+    if (j == my_strlen(av[2]) && i == my_strlen(av[1]))
         my_putchar('0');
     return (0);
 }
