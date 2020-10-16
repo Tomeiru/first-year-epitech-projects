b022916f83c5997049fd80824285bde6de8e2cc9
Mathieu Brujan <ken@localhost.localdomain>
2020-10-16 15:17:56 -0400

    100% NO NORM????

--- biggest_number.c
+++ biggest_number.c
@@ -15,13 +15,13 @@ int biggest_number (char *fst_num, char *sec_num, int fst_sign, int sec_sign)
             my_putchar('-');
             return (1);
         }
-        if (fst_num[i] < sec_num [i] && sec_sign == -1) {
+        if (fst_num[i] < sec_num[i] && sec_sign == -1) {
             my_putchar('-');
             return (-1);
         }
         if (fst_num[i] > sec_num[i] && fst_sign == 1)
             return (1);
-        if (fst_num[i] < sec_num [i] && sec_sign == 1)
+        if (fst_num[i] < sec_num[i] && sec_sign == 1)
             return (-1);
         if (fst_sign == 1 && sec_sign == 1)
             return (0);
