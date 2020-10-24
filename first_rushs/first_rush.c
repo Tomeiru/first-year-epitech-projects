9c84ad3b4a3ca96f0779d82598e210991af96e19
Mathieu Brujan <ken@localhost.localdomain>
2020-10-24 07:48:16 -0400

    ye

---
+++ first_rushs/first_rush.c
@@ -0,0 +1,62 @@
+/*
+** EPITECH PROJECT, 2020
+** rush
+** File description:
+** rush
+*/
+
+char *top_bot_line(int a, char *square)
+{
+    int amax = a;
+    int i = 0;
+
+    for (i; square[i] != '\0'; i++);
+    while (a != 0) {
+        if (a < amax && a > 1)
+            square[i] = '-';
+        if (a == amax || a == 1)
+            square[i] = 'o';
+        i++;
+        a--;
+    }
+    square[i] = '\n';
+    return (square);
+}
+
+char *between_lines(int b, int c, char *square, int i)
+{
+    int bmax = b;
+
+    for (c -= 2; c > 0; c-- && i++) {
+        if (b == bmax) {
+            square[i] = '|';
+            i++;
+        } 
+        for (b; b > 2; b--) {
+            square[i] = ' ';
+            i++;
+        }
+        b--;
+        if (b == 1) {
+            square[i] = '|';
+            i++;
+        }
+        b = bmax;
+        square[i] = '\n';
+    }
+    return (square);
+}
+
+char *rush(int x, int y)
+{
+    char *square = malloc(sizeof(char) * (y * (x + 1) + 1));
+    int i = 0;
+
+    square = top_bot_line(x, square);
+    for (i; square[i] != '\0'; i++);
+    square = between_lines(x, y, square, i);
+    if (y >= 2)
+        square = top_bot_line(x, square);
+    square[(y * (x + 1) + 1)] = '\0';
+    return (square);
+}
