40e370a4cb657cb445cec5132ff8d7c16d3260c4
Lilou <lilou@localhost.localdomain>
2020-10-26 16:56:37 +0100

    getnbr base

--- my_putnbr_base.c
+++ my_putnbr_base.c
@@ -5,16 +5,55 @@
 ** my_putnbr_base
 */
 
-int  my_putnbr_base(int nbr , char  const *base)
-{
-    int n = my_strlen(base);
+#include <stdio.h>
+#include <stdlib.h>
 
-    if (nbr < 0) {
-        nbr = nbr * -1;
+/*int  my_putnbr_base(char *nbr , char  const *base)
+{
+    int n = strlen(base);
+
+    if (nbr[0] == '-') {
         my_putchar('-');
+        nbr++;
     }
-    if (nbr > n) {
-        my_putnbr_base(nbr/n, base);
+    if (my_getnbr(nbr) > n) {
+        my_putnbr_base(infindiv(nbr/n), base);
     }
-    my_putchar(base[nbr % n]);
+    my_putchar(base[infinmod(nbr % n)]);
+    return (0);
+}*/
+
+int power_it (int len_base, int n)
+{
+    int res = 1;
+    for(int i = 1; i <= n; i ++) {
+        res = res * len_base;
+    }
+    return (res);
+}
+
+int my_getnbr_base(char *nbr, char *base, int len, int res)
+{
+    int len_base = my_strlen(base);
+    int i;
+    int n;
+
+    if (nbr[0] == '-')
+        my_putchar('-');
+    for (i = 0; base[i] != nbr[0]; i++);
+    n = i;
+    res += n * power_it(len_base, len);
+    if (len > 0) {
+        len--;
+        nbr++;
+        res = my_getnbr_base(nbr, base, len, res);
+    }
+    return (res);
+}
+
+int main (int ac, char**av)
+{
+    int res = my_getnbr_base(av[1], av[2], my_strlen(av[1]) - 1, res);
+    printf("%i\n", res);
+    return (0);
 }
