ed898d5f31ffd925958e4b22f731742cb6fb8ed2
Mathieu Brujan <ken@pc-112.home>
2020-10-31 14:09:42 -0400

    Pushing eval_expr that can manage a lot of '-'

--- eval_expr.c
+++ eval_expr.c
@@ -12,14 +12,13 @@ char *my_strtol(char *str, char **endptr, char *ops)
     char *res = malloc(10000);
     int i = 0;
     int j = 0;
+    int sign = 1;
 
-    if (str[i] == ops[3]) {
-        res[j++] = ops[3];
-        i++;
-    }
-    else if (str[i] == ops[2] || str[i] == ops[4] || str[i] == ops[5] ||
-    str[i] == ops[6])
-        i++;
+    for ( ;str[i] == ops[2] || str[i] == ops[3]; i++)
+        if (str[i] == ops[3])
+            sign *= -1;
+    if (sign == -1)
+        res[j++] = '-';
     for ( ; str[i] != '\0'; i++) {
         if (str[i] >= '0' && str[i] <= '9')
             res[j++] = str[i];
@@ -34,12 +33,16 @@ char *parentheses(char *str, char **endptr, char *ops, int size)
 {
     char *res = my_strtol(str, endptr, ops);
 
+    for ( ; str[0] == ops[3]; ++str);
     if ((str[0] == ops[2] || str[0] == ops[3] || str[0] == ops[4] ||
     str[0] == ops[5] || str[0] == ops[6]) && (str[1] == ops[0]))
         str++;
     if (*str == ops[0]) {
         str++;
-        res = summands(&str, ops, size);
+        if (res[0] == '-' && res[1] == '\0')
+            res = minus_adder(summands(&str, ops, size));
+        else
+            res = summands(&str, ops, size);
         *endptr = str;
         if (**endptr == ')')
             *endptr += 1;
@@ -52,12 +55,18 @@ char *factors(char *str, char **endptr, char *ops, int size)
     char *res = parentheses(str, endptr, ops, size);
 
     while (**endptr == ops[4] || **endptr == ops[5] || **endptr == ops[6]) {
-        if (**endptr == ops[4])
+        if (**endptr == ops[4]) {
+            *endptr += 1;
             res = infin_mult(res, parentheses(*endptr, endptr, ops, size));
-        else if (**endptr == ops[5])
+        }
+        else if (**endptr == ops[5]) {
+            *endptr += 1;
             res = infin_div(res, parentheses(*endptr, endptr, ops, size));
-        else
+        }
+        else {
+            *endptr += 1;
             res = infin_mod(res, parentheses(*endptr, endptr, ops, size));
+        }
     }
     return (res);
 }
@@ -68,10 +77,14 @@ char *summands(char **str_ptr, char *ops, int size)
     char *res = factors(*str_ptr, &endptr, ops, size);
 
     while (*endptr == ops[2] || *endptr == ops[3]) {
-        if (*endptr == ops[2])
+        if (*endptr == ops[2]) {
+            endptr += 1;
             res = infin_add(res, factors(endptr, &endptr, ops, size));
-        if (*endptr == ops[3])
+        }
+        if (*endptr == ops[3]) {
+            endptr += 1;
             res = infin_sub(res, factors(endptr, &endptr, ops, size));
+        }
     }
     *str_ptr = endptr;
     return (res);
