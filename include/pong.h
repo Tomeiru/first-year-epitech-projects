bb1eac9fb9b6b324076d6a0dcbf22a37c2df8cd1
Mathieu Brujan <ken@pc-112.home>
2020-11-06 06:07:44 -0500

    Obligatory things : include folder : adding an include folder with only one include currently : pong.h that will contains at the end all the prototypes and include of the 101pong project

---
+++ include/pong.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2020
+** 101pong
+** File description:
+** pong
+*/
+
+#ifndef PONG_H_
+#define PONG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+int main(int ac, char **av);
+int error(int ac, char **av);
+int pong(int ac, char **av);
+float answer_calc(float *vec_minus_one, float *vec_zero, int n);
+
+
+#endif /* !PONG_H_ */
