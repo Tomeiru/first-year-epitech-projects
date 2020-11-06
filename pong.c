d72d22a54c7068c879180ac6378cc4195e7f1109
Mathieu Brujan <ken@pc-112.home>
2020-11-06 06:11:23 -0500

    101pong : pong.c : adding the main function of the program that is going to call all the function needed for the project after an error handling that will return the value 84 to any error in the numbers given or the number of argument

---
+++ pong.c
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2020
+** 101pong
+** File description:
+** 101pong
+*/
+
+#include "pong.h"
+
+float *arg_to_vec(char **av, int nb_vec)
+{
+    float *res = malloc(sizeof(int) * 3);
+
+    if (nb_vec == 1) {
+        res[0] = atof(av[1]);
+        res[1] = atof(av[2]);
+        res[2] = atof(av[3]);
+    }
+    if (nb_vec == 2) {
+        res[0] = atof(av[4]);
+        res[1] = atof(av[5]);
+        res[2] = atof(av[6]);
+    }
+    return (res);
+}
+
+int pong(int ac, char **av)
+{
+    float *vec_minus_one;
+    float *vec_zero;
+    int n;
+
+    error(ac, av);
+    vec_minus_one = arg_to_vec(av, 1);
+    vec_zero = arg_to_vec(av, 2);
+    n = atoi(av[7]);
+    answer_calc(vec_minus_one, vec_zero, n);
+    return (0);
+}
