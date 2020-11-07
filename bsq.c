d3959b2f990ee46040d135e26b7c7e66cdb9d444
Mathieu Brujan <ken@pc-112.home>
2020-11-07 12:48:45 -0500

    BSQ : bsq.c : adding the main function of the program that is going to call all the function needed for the project after an error handling that will return the value 84 to all the bad file, format or board

--- bsq.c
+++ bsq.c
@@ -10,11 +10,16 @@
 int bsq(int ac, char **av)
 {
     char *board;
+    int line_num = 0;
 
     if (ac != 2)
         return (84);
     board = file_reader(av[1]);
     if (board[0] == '!')
         return (84);
+    line_num = my_get_first_line(board);
+    for ( ; *board != '\n'; *board++);
+    *board++;
+    algorithm(board, line_num);
     return (0);
 }
