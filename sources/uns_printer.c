0b8471e3c70c9b2faa28d46fd0c6e32bc70b3a89
Mathieu Brujan <ken@pc-112.home>
2020-11-14 16:05:32 -0500

    Unsigned int printer : uns_printer.c : Adding the file that contain the functions that will cast the number to the corresponding length modifier then determine which width modifier will be used and then call the function that will print the final result with the width modifier

--- sources/uns_printer.c
+++ sources/uns_printer.c
@@ -11,29 +11,37 @@ void nuns(unsigned long long number, char *flag)
 {
     unsigned int cast = number;
     char *nb = print_u(cast);
+    char *width_modifier = width_check(flag);
+    int length = my_strlen(nb);
 
-    my_putstr(nb);
+    print_width(width_modifier, length, nb, flag);
 }
 
 void huns(unsigned long long number, char *flag)
 {
     unsigned short cast = number;
     char *nb = print_u(cast);
+    char *width_modifier = width_check(flag);
+    int length = my_strlen(nb);
 
-    my_putstr(nb);
+    print_width(width_modifier, length, nb, flag);
 }
 
 void hhuns(unsigned long long number, char *flag)
 {
     unsigned char cast = number;
     char *nb = print_u(cast);
+    char *width_modifier = width_check(flag);
+    int length = my_strlen(nb);
 
-    my_putstr(nb);
+    print_width(width_modifier, length, nb, flag);
 }
 
 void lluns(unsigned long long number, char *flag)
 {
     char *nb = print_u(number);
+    char *width_modifier = width_check(flag);
+    int length = my_strlen(nb);
 
-    my_putstr(nb);
+    print_width(width_modifier, length, nb, flag);
 }
