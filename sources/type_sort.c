d602ac01f1514e5f2ad7450f04759cd56f9780fb
Mathieu Brujan <ken@pc-112.home>
2020-11-14 16:04:22 -0500

    Type sorter : type_sort.c : Adding the function that will sort the different conversion specifier by type then send the correct flag with the correct argument to the correct function with the help of the list

---
+++ sources/type_sort.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2020
+** B-PSU-100-STG-1-1-myprintf-mathieu.brujan
+** File description:
+** type_sort
+*/
+
+#include "my_printf.h"
+
+void type_sort(char *flag, va_list list)
+{
+    int type = which_type(my_revstr(flag)[0]);
+
+    if (type == 0)
+        arg_int(flag, va_arg(list, long long));
+    if (type == 1)
+        arg_uns_int(flag, va_arg(list, unsigned long long));
+    if (type == 2)
+        arg_str(flag, va_arg(list, char *));
+    if (type == 3)
+        my_putchar('%');
+}
