3420718aeb1d565ec12581fbd92e101ac35e43d3
Mathieu Brujan <ken@pc-112.home>
2020-11-14 15:48:49 -0500

    Int printer : int_printer.c : Adding the file that contains the functions that will cast the number to the corresponding length modifier then determine which width modifier will be used and then call the function that will print the final result with the width modifier

--- sources/int_printer.c
+++ sources/int_printer.c
@@ -11,29 +11,47 @@ void nint(long long number, char *flag)
 {
     int cast = number;
     char *nb = print_di(cast);
+    char *width_modifier = width_check(flag);
+    int length = my_strlen(nb);
 
-    my_putstr(nb);
+    print_width(width_modifier, length, nb, flag);
 }
 
 void hint(long long number, char *flag)
 {
     short cast = number;
     char *nb = print_di(cast);
+    char *width_modifier = width_check(flag);
+    int length = my_strlen(nb);
 
-    my_putstr(nb);
+    print_width(width_modifier, length, nb, flag);
 }
 
 void hhint(long long number, char *flag)
 {
     char cast = number;
     char *nb = print_di(cast);
+    char *width_modifier = width_check(flag);
+    int length = my_strlen(nb);
 
-    my_putstr(nb);
+    print_width(width_modifier, length, nb, flag);
 }
 
 void llint(long long number, char *flag)
 {
     char *nb = print_di(number);
+    char *width_modifier = width_check(flag);
+    int length = my_strlen(nb);
 
-    my_putstr(nb);
+    print_width(width_modifier, length, nb, flag);
+}
+
+void char_print(char number, char *flag)
+{
+    char nb[2];
+    char *width_modifier = width_check(flag);
+
+    nb[0] = number;
+    nb[1] = '\0';
+    print_width(width_modifier, 1, nb, flag);
 }
