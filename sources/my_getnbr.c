d3e15a2f3693cd583393e71c0b0e2affebcfb222
Mathieu Brujan <ken@pc-112.home>
2020-11-14 15:51:00 -0500

    Original library function : lib_function.c : Adding basic functions that come from the Pool's library that are used in a lot of instance in the program

---
+++ sources/my_getnbr.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2020
+** my_getnbr
+** File description:
+** my_getnbr
+*/
+
+#include "my_printf.h"
+
+int my_getnbr(char const *str)
+{
+    int i = 0;
+    int sign = 1;
+    long number = 0;
+
+    for (i = 0; str[i] != '\0'; i++)
+        if (str[i] == '-')
+            sign = sign * -1;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            number = number * 10;
+            number = number + str[i] - '0';
+        }
+    }
+    number = number * sign;
+    if (number > 2147483647 || number < -2147483647)
+        return (0);
+    return ((int)number);
+}
