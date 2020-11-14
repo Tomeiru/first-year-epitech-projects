66156a4e1ad394d6f92fde38de7fe33a3e722f67
Mathieu Brujan <ken@pc-112.home>
2020-11-14 15:45:39 -0500

    Flag finder algorithm : flag.c : Adding the file that contains the functions used to find which flag is used, it returns the flag in astring or a 'e' in case of an error

--- sources/flag.c
+++ sources/flag.c
@@ -18,19 +18,12 @@ static int flag_condition(char c)
     return (0);
 }
 
-static char *flag_checker(char *flag, char *conv_spe)
+static char *flag_checker(char *flag)
 {
     int number = 0;
     int i = 0;
 
     flag = my_revstr(flag);
-    for (i = 0; i < 7; i++)
-        if (conv_spe[i] == flag[0]) {
-            number = 1;
-            break;
-        }
-    if (number != 1 && my_strlen(flag) != 1)
-        return("e");
     if (number == 1) {
         for (i = 1; flag_condition(flag[i]) == 1 && flag[i] != '\0'; i++);
         if (flag[i] != '\0')
@@ -48,7 +41,7 @@ char *flag_finder(char *format)
     for (int j = 0; conv_spe[j]; j++) {
         if (conv_spe[j] == format[i]) {
             result[i + 1] = '\0';
-            result = flag_checker(result, conv_spe);
+            result = flag_checker(result);
             return (result);
             }
         if (j == 11) {
@@ -56,4 +49,5 @@ char *flag_finder(char *format)
             j = -1;
         }
     }
+    return ("0");
 }
