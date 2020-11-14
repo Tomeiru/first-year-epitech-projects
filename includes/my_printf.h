de78e1233e93122b56c753867aaf8ed0e4387b00
Mathieu Brujan <ken@pc-112.home>
2020-11-14 15:28:59 -0500

    Obligatory things : include folder : adding an include folder with only one include currently : my_printf.h that will contains at the end all the prototypes and include of the my_printf.h project

--- includes/my_printf.h
+++ includes/my_printf.h
@@ -12,7 +12,7 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-int my_printf(const char *format, ...);
+void my_printf(char *format, ...);
 int det_len_signed(long long nb);
 int det_len_unsigned_dec(unsigned long long nb);
 int det_len_unsigned_oct(unsigned long long nb);
@@ -24,9 +24,10 @@ char *print_u(unsigned long long nb);
 char which_letter_min(unsigned long long decimal);
 char *print_x(unsigned long long decimal);
 char which_letter_maj(unsigned long long decimal);
-char *print_X(unsigned long long decimal);
+char *print_xmaj(unsigned long long decimal);
 char *print_b(unsigned long long decimal);
-void print_S(char const *str);
+void print_alt_string(char *str, char *flag);
+void print_str(char *str, char *flag);
 char *my_strdup(char const *src);
 int my_strlen(char const *str);
 char *my_revstr(char *str);
@@ -40,7 +41,7 @@ void hhint(long long number, char *flag);
 void llint(long long number, char *flag);
 void arg_oct(unsigned long long number, char *flag);
 void arg_hexa(unsigned long long number, char *flag);
-void arg_HEXA(unsigned long long number, char *flag);
+void arg_hexamaj(unsigned long long number, char *flag);
 void arg_uns(unsigned long long number, char *flag);
 void arg_bin(unsigned long long number, char *flag);
 void noct(unsigned long long number, char *flag);
@@ -51,10 +52,10 @@ void nhexa(unsigned long long number, char *flag);
 void hhexa(unsigned long long number, char *flag);
 void hhhexa(unsigned long long number, char *flag);
 void llhexa(unsigned long long number, char *flag);
-void nHEXA(unsigned long long number, char *flag);
-void hHEXA(unsigned long long number, char *flag);
-void hhHEXA(unsigned long long number, char *flag);
-void llHEXA(unsigned long long number, char *flag);
+void nhexamaj(unsigned long long number, char *flag);
+void hhexamaj(unsigned long long number, char *flag);
+void hhhexamaj(unsigned long long number, char *flag);
+void llhexamaj(unsigned long long number, char *flag);
 void nuns(unsigned long long number, char *flag);
 void huns(unsigned long long number, char *flag);
 void hhuns(unsigned long long number, char *flag);
@@ -65,5 +66,16 @@ void hhbin(unsigned long long number, char *flag);
 void llbin(unsigned long long number, char *flag);
 int which_len_mod(char *flag);
 char *flag_finder(char *format);
+char *width_check(char *flag);
+void print_width(char *width_mod, int length, char *print, char *flag);
+void char_print(char number, char *flag);
+void type_sort(char *flag, va_list list);
+char *neg_int(char *print, int *nb);
+int my_getnbr(char const *str);
+void arg_int(char *flag, long long number);
+void arg_uns_int(char *flag, unsigned long long number);
+void arg_str(char *flag, char *str);
+int which_type(char c);
+
 
 #endif /* !MY_PRINTF_H_ */
