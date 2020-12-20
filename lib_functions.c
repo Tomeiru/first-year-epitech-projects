42b1fef6a02469725615de406a4ed5909ca2b27f
Alessandro Tosi <atosi@desktop-mr772d5.home>
2020-12-20 18:32:29 +0100

    removed bonus function from file and moved it to bonus folder

--- lib_functions.c
+++ lib_functions.c
@@ -55,18 +55,3 @@ char *my_strcat(char *dest, char const *src)
     dest_pointer[i] = '\0';
     return (dest);
 }
-
-void print_in_color(char *board)
-{
-    write(1, board, 52);
-    for (int i = 52; board[i] != '\0'; i++) {
-        if (board[i] == '.')
-            write(1, "\033[0;34m", 7);
-        else if (board[i] == 'x')
-            write(1, "\033[0;31m", 7);
-        if (board[i] == 'o')
-            write(1, "\033[0;33m", 7);
-        my_putchar(board[i]);
-        write(1, "\033[0m", 4);
-    }
-}
