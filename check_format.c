e53da194f4cb6d001df2d396ab5e0ca7510d32dc
Mathieu Brujan <ken@localhost.localdomain>
2020-12-19 22:03:13 -0500

    Norm

--- check_format.c
+++ check_format.c
@@ -9,26 +9,22 @@
 
 int check_format(char *pos_str)
 {
-    char letter_char[8] = {
-        2, 5, 10, 13, 18, 21, 26, 29
+    int letter_char[16] = {
+        2, 5, 10, 13, 18, 21, 26, 29, 3, 6, 11, 14, 19, 22, 27, 30
     };
-    char number_char[8] = {
-        3, 6, 11, 14, 19, 22, 27, 30
-    };
-    char double_points[8] = {
-        1, 4, 9, 12, 17, 20, 25, 28
-    };
-    char length[8] = {
-        0, 8, 16, 24, 7, 15, 23, 31
+    int double_points[16] = {
+        0, 8, 16, 24, 7, 15, 23, 31, 1, 4, 9, 12, 17, 20, 25, 28
     };
     int comp = 2;
 
-    for (int i = 0; i < 8; i++)
-        if ((pos_str[letter_char[i]] < 'A' || pos_str[letter_char[i]] > 'H') ||
-        (pos_str[double_points[i]] != ':') || 
-        (pos_str[number_char[i]] < '1' || pos_str[number_char[i]] > '9') ||
-        (i < 4 && pos_str[length[i]] != 48 + comp++) ||
-        (i > 3 && pos_str[length[i]] != '\n'))
+    for (int i = 0; i < 16; i++)
+        if ((i < 8 && (pos_str[letter_char[i]] < 'A' || 
+        pos_str[letter_char[i]] > 'H')) ||
+        (i > 7 && pos_str[double_points[i]] != ':') || 
+        (i > 7 && (pos_str[letter_char[i]] < '1' || 
+        pos_str[letter_char[i]] > '9')) ||
+        (i < 4 && pos_str[double_points[i]] != 48 + comp++) ||
+        (i > 3 && i < 8 && pos_str[double_points[i]] != '\n'))
             return (error_write(2));
     return (0);
 }
