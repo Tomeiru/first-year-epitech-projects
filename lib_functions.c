85b7c8cdb398d37c6df1236b8da635c47f7e5384
Mathieu Brujan <ken@DESKTOP-J3R1J88.home>
2021-01-31 17:36:12 -0500

    norm

--- lib_functions.c
+++ lib_functions.c
@@ -72,12 +72,3 @@ int is_number(char *str)
             return (0);
     return (1);
 }
-
-int is_alphanum(char *str)
-{
-    for (int i = 0; str[i]; i++)
-        if (!((str[i] >= '0' && str[i] <= '9') ||
-        (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')))
-            return (0);
-    return (1);
-}
--- main.c
+++ main.c
@@ -7,6 +7,16 @@
 
 #include "mysh.h"
 
+
+int is_alphanum(char *str)
+{
+    for (int i = 0; str[i]; i++)
+        if (!((str[i] >= '0' && str[i] <= '9') ||
+        (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')))
+            return (0);
+    return (1);
+}
+
 int main(int ac, char **av, char **env)
 {
     error(ac, av);
