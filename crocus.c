b610ae97758b5fa0e81e979d7503dacb46a0ef7a
Mathieu Brujan <ken@localhost.localdomain>
2021-04-01 10:24:11 -0400

    crocus.c

--- crocus.c
+++ crocus.c
@@ -10,8 +10,8 @@
 void crocus(info_t *info_struct)
 {
     void (* print_nbr[10])(info_t *, int) = {
-    write_zero, write_one, write_two, write_three, write_four,
-    write_five, write_six, write_seven, write_eight, write_nine,
+        write_zero, write_one, write_two, write_three, write_four,
+        write_five, write_six, write_seven, write_eight, write_nine,
     };
 
     for (int i = 0; info_struct->numbers[i]; i++)
