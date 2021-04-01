3cde9595dba722cde4b632c641175c0ffbac17d4
Lilou Stoll-Metz <lilou.stoll-metz@epitech.eu>
2021-04-01 15:37:40 +0200

    correcting modulos

--- write_numbers_first.c
+++ write_numbers_first.c
@@ -25,67 +25,67 @@ void write_zero(info_t *info, int i)
 
 void write_one(info_t *info, int i)
 {
-    info->array[0][i + 1] = info->chars[info->lenght_chars % 1];
-    info->array[0][i + 2] = info->chars[info->lenght_chars % 1];
-    info->array[0][i + 3] = info->chars[info->lenght_chars % 1];
-    info->array[1][i] = info->chars[info->lenght_chars % 1];
-    info->array[1][i + 2] = info->chars[info->lenght_chars % 1];
-    info->array[1][i + 3] = info->chars[info->lenght_chars % 1];
-    info->array[2][i + 2] = info->chars[info->lenght_chars % 1];
-    info->array[2][i + 3] = info->chars[info->lenght_chars % 1];
-    info->array[3][i + 2] = info->chars[info->lenght_chars % 1];
-    info->array[3][i + 3] = info->chars[info->lenght_chars % 1];
-    info->array[4][i + 1] = info->chars[info->lenght_chars % 1];
-    info->array[4][i + 2] = info->chars[info->lenght_chars % 1];
-    info->array[4][i + 3] = info->chars[info->lenght_chars % 1];
-    info->array[4][i + 4] = info->chars[info->lenght_chars % 1];
+    info->array[0][i + 1] = info->chars[1 % info->lenght_chars];
+    info->array[0][i + 2] = info->chars[1 % info->lenght_chars];
+    info->array[0][i + 3] = info->chars[1 % info->lenght_chars];
+    info->array[1][i] = info->chars[1 % info->lenght_chars];
+    info->array[1][i + 2] = info->chars[1 % info->lenght_chars];
+    info->array[1][i + 3] = info->chars[1 % info->lenght_chars];
+    info->array[2][i + 2] = info->chars[1 % info->lenght_chars];
+    info->array[2][i + 3] = info->chars[1 % info->lenght_chars];
+    info->array[3][i + 2] = info->chars[1 % info->lenght_chars];
+    info->array[3][i + 3] = info->chars[1 % info->lenght_chars];
+    info->array[4][i + 1] = info->chars[1 % info->lenght_chars];
+    info->array[4][i + 2] = info->chars[1 % info->lenght_chars];
+    info->array[4][i + 3] = info->chars[1 % info->lenght_chars];
+    info->array[4][i + 4] = info->chars[1 % info->lenght_chars];
 }
 
 void write_two(info_t *info, int i)
 {
-    info->array[0][i + 1] = info->chars[info->lenght_chars % 2];
-    info->array[0][i + 2] = info->chars[info->lenght_chars % 2];
-    info->array[0][i + 3] = info->chars[info->lenght_chars % 2];
-    info->array[1][i] = info->chars[info->lenght_chars % 2];
-    info->array[1][i + 4] = info->chars[info->lenght_chars % 2];
-    info->array[2][i + 3] = info->chars[info->lenght_chars % 2];
-    info->array[3][i + 1] = info->chars[info->lenght_chars % 2];
-    info->array[4][i] = info->chars[info->lenght_chars % 2];
-    info->array[4][i + 1] = info->chars[info->lenght_chars % 2];
-    info->array[4][i + 2] = info->chars[info->lenght_chars % 2];
-    info->array[4][i + 3] = info->chars[info->lenght_chars % 2];
-    info->array[4][i + 4] = info->chars[info->lenght_chars % 2];
+    info->array[0][i + 1] = info->chars[2 % info->lenght_chars];
+    info->array[0][i + 2] = info->chars[2 % info->lenght_chars];
+    info->array[0][i + 3] = info->chars[2 % info->lenght_chars];
+    info->array[1][i] = info->chars[2 % info->lenght_chars];
+    info->array[1][i + 4] = info->chars[2 % info->lenght_chars];
+    info->array[2][i + 3] = info->chars[2 % info->lenght_chars];
+    info->array[3][i + 1] = info->chars[2 % info->lenght_chars];
+    info->array[4][i] = info->chars[2 % info->lenght_chars];
+    info->array[4][i + 1] = info->chars[2 % info->lenght_chars];
+    info->array[4][i + 2] = info->chars[2 % info->lenght_chars];
+    info->array[4][i + 3] = info->chars[2 % info->lenght_chars];
+    info->array[4][i + 4] = info->chars[2 % info->lenght_chars];
 }
 
 void write_three(info_t *info, int i)
 {
-    info->array[0][i] = info->chars[info->lenght_chars % 3];
-    info->array[0][i + 1] = info->chars[info->lenght_chars % 3];
-    info->array[0][i + 2] = info->chars[info->lenght_chars % 3];
-    info->array[0][i + 3] = info->chars[info->lenght_chars % 3];
-    info->array[1][i + 4] = info->chars[info->lenght_chars % 3];
-    info->array[2][i + 1] = info->chars[info->lenght_chars % 3];
-    info->array[2][i + 2] = info->chars[info->lenght_chars % 3];
-    info->array[2][i + 3] = info->chars[info->lenght_chars % 3];
-    info->array[3][i + 4] = info->chars[info->lenght_chars % 3];
-    info->array[4][i] = info->chars[info->lenght_chars % 3];
-    info->array[4][i + 1] = info->chars[info->lenght_chars % 3];
-    info->array[4][i + 2] = info->chars[info->lenght_chars % 3];
-    info->array[4][i + 3] = info->chars[info->lenght_chars % 3];
+    info->array[0][i] = info->chars[3 % info->lenght_chars % 3];
+    info->array[0][i + 1] = info->chars[3 % info->lenght_chars];
+    info->array[0][i + 2] = info->chars[3 % info->lenght_chars];
+    info->array[0][i + 3] = info->chars[3 % info->lenght_chars];
+    info->array[1][i + 4] = info->chars[3 % info->lenght_chars];
+    info->array[2][i + 1] = info->chars[3 % info->lenght_chars];
+    info->array[2][i + 2] = info->chars[3 % info->lenght_chars];
+    info->array[2][i + 3] = info->chars[3 % info->lenght_chars];
+    info->array[3][i + 4] = info->chars[3 % info->lenght_chars];
+    info->array[4][i] = info->chars[3 % info->lenght_chars];
+    info->array[4][i + 1] = info->chars[3 % info->lenght_chars];
+    info->array[4][i + 2] = info->chars[3 % info->lenght_chars];
+    info->array[4][i + 3] = info->chars[3 % info->lenght_chars];
 }
 
 void write_four(info_t *info, int i)
 {
-    info->array[0][i + 2] = info->chars[info->lenght_chars % 4];
-    info->array[0][i + 3] = info->chars[info->lenght_chars % 4];
-    info->array[1][i + 1] = info->chars[info->lenght_chars % 4];
-    info->array[1][i + 3] = info->chars[info->lenght_chars % 4];
-    info->array[2][i + 0] = info->chars[info->lenght_chars % 4];
-    info->array[2][i + 3] = info->chars[info->lenght_chars % 4];
-    info->array[3][i] = info->chars[info->lenght_chars % 4];
-    info->array[3][i + 1] = info->chars[info->lenght_chars % 4];
-    info->array[3][i + 2] = info->chars[info->lenght_chars % 4];
-    info->array[3][i + 3] = info->chars[info->lenght_chars % 4];
-    info->array[3][i + 4] = info->chars[info->lenght_chars % 4];
-    info->array[4][i + 3] = info->chars[info->lenght_chars % 4];
+    info->array[0][i + 2] = info->chars[4 % info->lenght_chars];
+    info->array[0][i + 3] = info->chars[4 % info->lenght_chars];
+    info->array[1][i + 1] = info->chars[4 % info->lenght_chars];
+    info->array[1][i + 3] = info->chars[4 % info->lenght_chars];
+    info->array[2][i + 0] = info->chars[4 % info->lenght_chars];
+    info->array[2][i + 3] = info->chars[4 % info->lenght_chars];
+    info->array[3][i] = info->chars[4 % info->lenght_chars];
+    info->array[3][i + 1] = info->chars[4 % info->lenght_chars];
+    info->array[3][i + 2] = info->chars[4 % info->lenght_chars];
+    info->array[3][i + 3] = info->chars[4 % info->lenght_chars];
+    info->array[3][i + 4] = info->chars[4 % info->lenght_chars];
+    info->array[4][i + 3] = info->chars[4 % info->lenght_chars];
 }
