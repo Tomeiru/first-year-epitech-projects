5e523852b433e94b01af3b85e6660b767c48433f
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2021-04-28 16:04:45 +0200

    fix misstyping

--- src/elements/entities/particles/meteo_effects.c
+++ src/elements/entities/particles/meteo_effects.c
@@ -45,7 +45,7 @@ sfVector2f top_left, sfVector2f player_pos, float darkness)
                 (player_screen_pos.x + x) / BUFFER_SCALE,
                 (player_screen_pos.y + y) / BUFFER_SCALE};
             if (buffer_pos.x >= buffer->size.x ||
-            buffer_pos.y >= buffer->pos.y)
+            buffer_pos.y >= buffer->size.y)
                 continue;
             buffer_put_pixel(buffer, light, buffer_pos);
         }
