aaa4dc68690042d4dae5b77f04c7b4fcf821b95a
Criticorn <alessandro.tosi@epitech.eu>
2021-05-03 14:21:07 +0200

    Updated dialogue handling

--- src/elements/entities/dialogue.c
+++ src/elements/entities/dialogue.c
@@ -9,19 +9,28 @@
 #include "my.h"
 #include "elements/entities/dialogue.h"
 
-void dialogue_draw(dialogue_t *dialogue, sfRenderWindow *window)
+void dialogue_update(dialogue_t *dialogue, infos_t *infos, float elapsed)
 {
     char temp = dialogue->str[dialogue->pos];
 
     dialogue->str[dialogue->pos] = '\0';
     sfText_setString(dialogue->text, dialogue->str);
-    text_draw(dialogue, window);
+    text_draw(dialogue, infos->window);
     dialogue->str[dialogue->pos] = temp;
     if (dialogue->pos != dialogue->len){
-        dialogue.pos += 2;
+        dialogue->pos += (int) 2 * elapsed;
         if (dialogue.pos > dialogue->len)
             dialogue->pos = dialogue->len;
     }
+}
+
+void dialogue_set_fcts(dialogue_t *dialogue)
+{
+    dialogue->move = &text_move;
+    dialogue->rotate = &text_rotate;
+    dialogue->draw = &text_draw;
+    dialogue->destroy = &text_destroy;
+    dialogue->update = &dialogue_update;
 }
 
 dialogue_t *dialogue_create(char *text_str, sfFont *font, sfVector2f pos,
@@ -38,15 +47,11 @@ int size)
     sfText_setCharacterSize(text_sfml, size);
     dialogue->sprite = NULL;
     dialogue->text = text_sfml;
-    dialogue->move = &text_move;
-    dialogue->rotate = &text_rotate;
-    dialogue->draw = &text_draw;
-    dialogue->destroy = &text_destroy;
     dialogue->time = 0;
     dialogue->pos = 1;
     dialogue->str = my_strdup(text_str);
     dialogue->len = my_strlen(text_str);
     element_set_hitbox((element_t*) dialogue, sfText_getGlobalBounds(text_sfml));
     text_move((element_t*) dialogue, pos);
-    return (text);
+    return (dialogue);
 }
