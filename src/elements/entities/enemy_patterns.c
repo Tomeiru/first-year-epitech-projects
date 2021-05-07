04fba8ffc73c0aa2a2d92debe22eefed9e4c56a9
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2021-05-07 19:16:52 +0200

    patterns: enemy dist can be negative

--- src/elements/entities/enemy_patterns.c
+++ src/elements/entities/enemy_patterns.c
@@ -14,28 +14,34 @@ void enemy_vertical_pattern(enemy_t *enemy,
 sfVector2f *move, infos_t *infos, float speed)
 {
     UNUSED(infos);
-    if (enemy->move_status == 1
-    && enemy->pos.y > enemy->pos_start.y + enemy->dist)
+    if (enemy->move_status == 1 &&
+    ((enemy->dist > 0 && enemy->pos.y > enemy->pos_start.y + enemy->dist)
+    || (enemy->dist < 0 && enemy->pos.y < enemy->pos_start.y + enemy->dist)))
         enemy->move_status = 2;
-    if (enemy->move_status == 2 && enemy->pos.y < enemy->pos_start.y)
+    if (enemy->move_status == 2 &&
+    ((enemy->dist > 0 && enemy->pos.y < enemy->pos_start.y)
+    || (enemy->dist < 0 && enemy->pos.y > enemy->pos_start.y)))
         enemy->move_status = 1;
     if (enemy->move_status == 1)
-        *move = (sfVector2f) {0, speed};
+        *move = (sfVector2f) {0, enemy->dist > 0 ? speed : -speed};
     if (enemy->move_status == 2)
-        *move = (sfVector2f) {0, -speed};
+        *move = (sfVector2f) {0, enemy->dist > 0 ? -speed : speed};
 }
 
 void enemy_horizonthal_pattern(enemy_t *enemy,
 sfVector2f *move, infos_t *infos, float speed)
 {
     UNUSED(infos);
-    if (enemy->move_status == 1
-    && enemy->pos.x > enemy->pos_start.x + enemy->dist)
+    if (enemy->move_status == 1 &&
+    ((enemy->dist > 0 && enemy->pos.x > enemy->pos_start.x + enemy->dist)
+    || (enemy->dist < 0 && enemy->pos.x < enemy->pos_start.x + enemy->dist)))
         enemy->move_status = 2;
-    if (enemy->move_status == 2 && enemy->pos.x < enemy->pos_start.x)
+    if (enemy->move_status == 2 &&
+    ((enemy->dist > 0 && enemy->pos.x < enemy->pos_start.x)
+    || (enemy->dist < 0 && enemy->pos.x > enemy->pos_start.x)))
         enemy->move_status = 1;
     if (enemy->move_status == 1)
-        *move = (sfVector2f) {speed, 0};
+        *move = (sfVector2f) {enemy->dist > 0 ? speed : -speed, 0};
     if (enemy->move_status == 2)
-        *move = (sfVector2f) {-speed, 0};
+        *move = (sfVector2f) {enemy->dist > 0 ? -speed : speed, 0};
 }
